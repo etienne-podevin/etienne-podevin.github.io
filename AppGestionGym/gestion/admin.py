92ea128733ea503a534dba2dee3d10551d0afbf6
etienne-podevin <etienne-podevin.pro@outlook.fr>
2023-02-27 15:59:45 +0100

    deletion de gym

--- AppGestionGym/gestion/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Personne, Adherent, Encadrant, Groupe, Cours
-
-admin.site.register(Personne)
-admin.site.register(Adherent)
-admin.site.register(Encadrant)
-admin.site.register(Groupe)
-admin.site.register(Cours)
--- AppGestionGym/gestion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('groupe', views.groupe, name='groupe'),
-    path('adherent', views.adherent, name='adherent'),
-    path('encadrant', views.encadrant, name='encadrant'),
-]
